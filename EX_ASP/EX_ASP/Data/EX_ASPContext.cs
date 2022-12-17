04818724a4c36ddffa7591476ff53f49f38357a1
maiduyquan <91303493+maiduyquan@users.noreply.github.com>
2022-12-17 13:55:38 +0700

    ok

---
+++ EX_ASP/EX_ASP/Data/EX_ASPContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EX_ASP.Models;
+
+namespace EX_ASP.Data
+{
+    public class EX_ASPContext : DbContext
+    {
+        public EX_ASPContext (DbContextOptions<EX_ASPContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EX_ASP.Models.Employee> Employee { get; set; } = default!;
+
+    }
+}
---
+++ EX_ASP/EX_ASP/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EX_ASP.Models;
+using System;
+using System.Linq;
+using EX_ASP.Data;
+
+namespace EX_ASP.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+		{
+			using (var context = new EX_ASPContext(
+				serviceProvider.GetRequiredService<
+					DbContextOptions<EX_ASPContext>>()))
+			{
+				// Look for any movies.
+				if (context.Employee.Any())
+				{
+					return;   // DB has been seeded
+				}
+				context.Employee.AddRange(
+					new Employee
+					{
+						EmployeeId = "EM001",
+						EmployeeName = "Jonh Carter",
+						Department = "HR",
+						Salary = 3000
+					},
+					new Employee
+					{
+						EmployeeId = "EM002",
+						EmployeeName = "Michael Bean",
+						Department = "SC",
+						Salary = 1300
+					},
+					new Employee
+					{
+						EmployeeId = "EM003",
+						EmployeeName = "Jimmy Floy",
+						Department = "MD",
+						Salary = 2000
+					},
+					new Employee
+					{
+						EmployeeId = "EM004",
+						EmployeeName = "Mary Brown",
+						Department = "MD",
+						Salary = 2000
+					}
+				);
+				context.SaveChanges();
+			}
+		}
+    }
+}
